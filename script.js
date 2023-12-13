5a6dda12062ae9454cf41cf7e66102938f6b64e7
terry-source <pendalterry@gmail.com>
2023-12-13 12:02:32 +1100

    add feat word shifts

--- script.js
+++ script.js
@@ -2,103 +2,228 @@
 const targetWordElement = document.getElementById("target-word");
 const synonymListElement = document.getElementById("synonym-list");
 const gameBoardElement = document.getElementById("game-board");
-const playAgainButton = document.getElementById("play-again-button");
+const resetButton = document.getElementById("reset-button");
+
+/*----- constants -----*/
+
+const fastSpeed = 1;
+const slowSpeed = 0.5;
 
 /*----- state variables -----*/
 
 let targetWord = "elegant";
 let synonyms = [
-    "graceful",
-    "handsome",
-    "fine",
-    "luxurious",
-    "majestic",
-    "magnificent",
-    "classy",
-    "stylish",
-    "simple",
-    "tasteful",
-    "glorious",
-    "proud",
-    "royal",
-    "superb",
-    "regal",
-    "refined",
-    "rich",
-    "sophisticated",
-    "aristocratic",
-    "noble",
-    "gallant",
-    "courtly",
-    "stately",
+  "graceful",
+  "handsome",
+  "fine",
+  "luxurious",
+  "majestic",
+  "magnificent",
+  "classy",
+  "stylish",
+  "simple",
+  "tasteful",
+  "glorious",
+  "proud",
+  "royal",
+  "superb",
+  "regal",
+  "refined",
+  "rich",
+  "sophisticated",
+  "aristocratic",
+  "noble",
+  "gallant",
+  "courtly",
+  "stately",
 ];
 let antonyms = [
-    "tasteless",
-    "dowdy",
-    "graceless",
-    "loud",
-    "grotesque",
-    "flamboyant",
-    "flashy",
-    "gaudy",
-    "crude",
-    "coarse",
-    "tawdry",
-    "glitzy",
-    "tacky",
-    "garish",
-    "cheesy",
-    "raffish",
-    "splashy",
-    "styleless",
-    "vulgar",
-    "trashy",
-    "rude",
-    "rough-hewn",
-    "ticky-tacky",
-    "ticky-tack",
-    "rough-edged",
-    "tasteless",
-    "dowdy",
-    "graceless",
-    "loud",
-    "grotesque",
-    "flamboyant",
-    "flashy",
-    "gaudy",
-    "crude",
-    "coarse",
-    "tawdry",
-    "glitzy",
-    "tacky",
-    "garish",
-    "cheesy",
-    "raffish",
-    "splashy",
-    "styleless",
-    "vulgar",
-    "uncouth",
-    "trashy",
-    "rude",
-    "rough-hewn",
-    "ticky-tacky",
-    "ticky-tack",
-    "rough-edged",
+  "tasteless",
+  "dowdy",
+  "graceless",
+  "loud",
+  "grotesque",
+  "flamboyant",
+  "flashy",
+  "gaudy",
+  "crude",
+  "coarse",
+  "tawdry",
+  "glitzy",
+  "tacky",
+  "garish",
+  "cheesy",
+  "raffish",
+  "splashy",
+  "styleless",
+  "vulgar",
+  "trashy",
+  "rude",
+  "rough-hewn",
+  "ticky-tacky",
+  "ticky-tack",
+  "rough-edged",
+  "tasteless",
+  "dowdy",
+  "graceless",
+  "loud",
+  "grotesque",
+  "flamboyant",
+  "flashy",
+  "gaudy",
+  "crude",
+  "coarse",
+  "tawdry",
+  "glitzy",
+  "tacky",
+  "garish",
+  "cheesy",
+  "raffish",
+  "splashy",
+  "styleless",
+  "vulgar",
+  "uncouth",
+  "trashy",
+  "rude",
+  "rough-hewn",
+  "ticky-tacky",
+  "ticky-tack",
+  "rough-edged",
 ];
 
 /*----- functions -----*/
 
-class WordShooterGame {
-    constructor(targetWord, synonyms, randomWords) {
-        this.targetWord = targetWord;
-        this.synonyms = synonyms;
-        this.randomWords = randomWords;
-        this.score = 0;
-        this.remainingTime = 20;
+class WordShooter {
+  constructor(targetWord, synonyms, randomWords) {
+    this.targetWord = targetWord;
+    this.synonyms = synonyms;
+    this.randomWords = randomWords;
+    this.score = 0;
+    this.remainingTime = 20;
+  }
+
+  startGame() {
+    // show target word
+    targetWordElement.textContent = this.targetWord;
+
+    // combine lists
+    this.combinedList = this.combineWordList();
+
+    // create word list
+    synonymListElement.innerHTML = "";
+    for (const synonym of this.combinedList) {
+      const synonymItem = document.createElement("li");
+      synonymItem.textContent = synonym;
+      synonymListElement.appendChild(synonymItem);
+
+      // add event listener to each word
+      synonymItem.addEventListener("click", this.handleSynonymClick.bind(this));
+
+      // random positions of words
+      synonymItem.style.left =
+        Math.random() * gameBoardElement.offsetWidth + "px";
+      synonymItem.style.top =
+        Math.round((Math.random() * gameBoardElement.offsetHeight) / 12) * 12 +
+        "px";
     }
 
+    // move words
+    this.moveSynonyms();
+
+    // countdown timer
+    this.setTimer(() => {
+      // reset game
+      this.reset();
+    });
+  }
+
+  combineWordList() {
+    const combinedArray = [...this.synonyms, ...this.randomWords];
+    combinedArray.sort(() => Math.random() - 0.5);
+    console.log(combinedArray);
+    return combinedArray.slice(0, 10);
+  }
+
+  isPositionOverlapping(newLeft, newTop, existingElements) {
+    for (const existingItem of existingElements) {
+      const existingLeft = existingItem.style.left;
+      const existingTop = existingItem.style.top;
+
+      const existingRight = parseFloat(existingLeft) + existingItem.offsetWidth;
+      const existingBottom =
+        parseFloat(existingTop) + existingItem.offsetHeight;
+
+      if (
+        parseFloat(newLeft) < existingRight &&
+        parseFloat(newRight) > existingLeft &&
+        parseFloat(newTop) < existingBottom &&
+        parseFloat(existingBottom) > existingTop
+      ) {
+        return true; // Overlap detected
+      }
+    }
+
+    return false; // No overlap found
+  }
+
+  setTimer(resetCallback) {
+    const timerInterval = setInterval(() => {
+      if (this.remainingTime > 0) {
+        this.remainingTime--;
+        document.getElementById("timer").textContent = this.remainingTime;
+      } else {
+        clearInterval(timerInterval);
+        resetCallback();
+      }
+    }, 1000);
+  }
+
+  reset() {
+    // show reset button
+    document.getElementById("reset-button").style.display = "block";
+
+    // stop moving words
+    window.cancelAnimationFrame(moveSynonyms);
+
+    // show score
+    alert(`Round Complete! Final score is: ${this.score}`);
+  }
+
+  moveSynonyms() {
+    for (const synonymItem of synonymListElement.children) {
+      // random speed
+      const randomSpeed = Math.random() * 0.5 + 0.1;
+
+      //   // Update position based on random speed
+      //   synonymItem.style.left =
+      //     parseFloat(synonymItem.style.left) + randomSpeed + "px";
+
+      requestAnimationFrame(() => {
+        synonymItem.style.left = parseFloat(synonymItem.style.left) + 1 + "px";
+        synonymItem.style.transitionTimingFunction = "ease";
+      });
+
+      // Check for element reaching edge of game area and reset
+      if (parseFloat(synonymItem.style.left) > gameBoardElement.offsetWidth) {
+        synonymItem.style.left = 0;
+      }
+    }
+
+    // Repeat animation loop
+    window.requestAnimationFrame(this.moveSynonyms.bind(this));
+  }
+
+  handleSynonymClick(event) {
+    const clickedSynonym = event.target.textContent;
+
+    // Check if clicked synonym is correct
+    if (clickedSynonym in synonyms) {
+      this.score++;
+      event.target.remove(); // Remove clicked element
+    }
+  }
 }
 
 // Start the first game
-const firstGame = new WordShooterGame(targetWord, synonyms, antonyms);
+const firstGame = new WordShooter(targetWord, synonyms, antonyms);
 firstGame.startGame();
